875acdf09614801dd37405f723f9dfd662d48b5b
Joaoof <joaodeus400@gmail.com>
2023-03-29 09:56:46 -0300

    Commit: Banco de dados - Criando primeira migration

--- src/database.ts
+++ src/database.ts
@@ -1,8 +1,15 @@
-import { knex as setupKnex } from 'knex'
+import { knex as setupKnex, Knex } from 'knex'
 
-export const knex = setupKnex({
+export const config: Knex.Config = {
   client: 'sqlite',
   connection: {
-    filename: './tmp/app.db', // nome do arq do banco de dados onde ele vai ser salvo
+    filename: './db/app.db', // nome do arq do banco de dados onde ele vai ser salvo
   },
-})
+  useNullAsDefault: true,
+  migrations: {
+    extension: 'ts',
+    directory: './db/migrations',
+  },
+}
+
+export const knex = setupKnex(config)
