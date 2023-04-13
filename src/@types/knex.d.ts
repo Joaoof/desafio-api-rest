fc051f421d90cf71584cb7bcee906faa6f94858c
Joaoof <joaodeus400@gmail.com>
2023-04-13 02:38:05 -0300

    Tipagem no Knex

---
+++ src/@types/knex.d.ts
@@ -0,0 +1,15 @@
+// eslint-disable-next-line
+import { Knex } from 'knex'
+
+declare module 'knex/types/tables' {
+  export interface Tables {
+    meals: {
+      id: string
+      name: string
+      email: string
+      password_hash: string
+      created_at: string
+      session_id?: string
+    }
+  }
+}
