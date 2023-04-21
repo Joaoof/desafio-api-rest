5bdb228f65df5ed4fedd0fb77fc9b86b224f801c
Joaoof <joaodeus400@gmail.com>
2023-04-21 15:05:01 -0300

    teste de listagens de transações

--- src/env/test/meals.spec.ts
+++ src/env/test/meals.spec.ts
@@ -1,4 +1,4 @@
-import { it, beforeAll, afterAll, describe } from 'vitest'
+import { it, beforeAll, afterAll, describe, expect } from 'vitest'
 import request from 'supertest'
 import { app } from '../../app'
 
@@ -11,7 +11,7 @@ describe('Meals routes', () => {
     await app.close()
   })
 
-  it('o usuário consegue criar uma refeição', async () => {
+  it('should be able to create a new user', async () => {
     await request(app.server)
       .post('/meals')
       .send({
@@ -20,5 +20,28 @@ describe('Meals routes', () => {
         password: 'senha123',
       })
       .expect(201)
+  })
+
+  it('should be able to list all meals', async () => {
+    const createMealsResponse = await request(app.server).post('/meals').send({
+      name: 'João de Deus',
+      email: 'joaodeus400@gmail.com',
+      password: 'senha123',
+    })
+
+    const cookies = createMealsResponse.get('Set-Cookie')
+
+    const listMealsResponse = await request(app.server)
+      .get('/meals')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(listMealsResponse.body.meal).toEqual([
+      expect.objectContaining({
+        name: 'João de Deus',
+        email: 'joaodeus400@gmail.com',
+        password: 'senha123',
+      }),
+    ])
   })
 })
