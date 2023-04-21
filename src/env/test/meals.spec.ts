e5d61394a5096661d9cf2c9a26fcf7ce7630246f
Joaoof <joaodeus400@gmail.com>
2023-04-21 03:56:48 -0300

    categorizando testes

---
+++ src/env/test/meals.spec.ts
@@ -0,0 +1,24 @@
+import { it, beforeAll, afterAll, describe } from 'vitest'
+import request from 'supertest'
+import { app } from '../../app'
+
+describe('Meals routes', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('o usuário consegue criar uma refeição', async () => {
+    await request(app.server)
+      .post('/meals')
+      .send({
+        name: 'João de Deus',
+        email: 'joaodeus400@gmail.com',
+        password: 'senha123',
+      })
+      .expect(201)
+  })
+})
